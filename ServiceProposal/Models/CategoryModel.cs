7ecb02be0789811a3445ddab5ff485b08011f846
KevinPozitive <marshal.vlad.99@gmail.com>
2020-06-11 14:20:07 +0300

    Fix problem

---
+++ ServiceProposal/Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class CategoryModel
+    {
+        //id категории
+        public int Id { get; set; }
+        //название категории
+        public string Name { get; set; }
+    }
+}
