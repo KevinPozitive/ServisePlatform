ab396ef26313d1d6a7cac81c1791410e0a788a88
KevinPozitive <marshal.vlad.99@gmail.com>
2020-06-14 23:54:52 +0300

    Learning and testing Code-First Database Design with Entity Framework (LocalDb).

--- ServiceProposal/Global.asax.cs
+++ ServiceProposal/Global.asax.cs
@@ -1,17 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ServiceProposal.Models;
 
 namespace ServiceProposal
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            using (CategoryContext db = new CategoryContext())
+            {
+                // создаем два объекта User
+                Category category1 = new Category { Name = "Дизайн и полиграфия" };
+                Category category2 = new Category { Name = "Пример" };
+
+                // добавляем их в бд
+                db.Categories.Add(category1);
+                db.Categories.Add(category2);
+                db.SaveChanges();     
+            }
+
+
+            Database.SetInitializer(new CategoryContext.CategoryInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
