fe5012ae573bc9e24e70499c8d81db8676b1aeb2
KevinPozitive <marshal.vlad.99@gmail.com>
2020-06-23 16:02:51 +0300

    Finalization of the popup window with tabs (authorization and registration). The relationship between the project and the database for authorization and registration

---
+++ ServiceProposal/Models/Provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceProposal.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Password { get; set; }
+
+        public string TelNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ ServiceProposal/Models/ProviderContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ServiceProposal.Models
+{
+    public class ProviderContext : DbContext
+    {
+        public ProviderContext()
+           : base("ProviderContext")
+        { }
+        public DbSet<Provider> Providers { get; set; }
+    }
+}
